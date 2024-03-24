0f705b52ff46e5f95ad2272f0ae1c20cb2146c71
thipbadee <146174087+thipbadee@users.noreply.github.com>
2024-03-24 20:50:31 +0700

    fix user friendly in cart

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-    matcher: ["/reservations/manage", "/reservations"]
+    matcher: ["/reservations/manage", "/reservations", "/cart"]
 }

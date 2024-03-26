2dfd1156ea2ce7023bce4950d1a0f6fd49d57ddb
Amphi <6633287021@student.chula.ac.th>
2024-03-26 19:27:56 +0700

    admin secret

--- src/app/reservations/manage/@dashboard/page.tsx
+++ src/app/reservations/manage/@dashboard/page.tsx
@@ -53,7 +53,7 @@ export default async function DashboardPage() {
     var createdAt = new Date(profile.data.createdAt)
 
     return(
-        <main className="bg-red-300 m-5 p-5 font-bold flex items-center text-center justify-center">
+        <main className="bg-rose-100 py-10 font-bold flex items-center text-center justify-center">
             {/* <div className="text-2xl">{profile.data.name}</div>
             <table className="table-auto border-separate border-spacing-2"><tbody>
                 <tr><td>Email</td><td>{profile.data.email}</td></tr>
@@ -63,95 +63,100 @@ export default async function DashboardPage() {
 
             {
                 (profile.data.role=="admin")?
+                <div>
+                    <div className="text-2xl font-bold mb-5 text-rose-300">Welcome {profile.data.name}!</div>
+                <div className="bg-white px-20 py-10 text-xl text-white rounded-xl">
                 <form action={addCar}>
-                    <div className="text-xl text-white">Create Car Model</div>
+                    <div className="text-2xl font-bold mb-5 text-rose-400">Create Car Model</div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="brand">
                             Brand</label> */}
                         <input type="text" required id="brand" name="brand" placeholder="Brand"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="carModel">
                             Car Model</label> */}
                         <input type="text" required id="carModel" name="carModel" placeholder="Car Model"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="type">
                             Type (ex. Sedan, Pickup etc.)</label> */}
                         <input type="text" required id="type" name="type" placeholder="Type"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="pricePerDay">
                             Price per day</label> */}
-                        <input type="text" required id="pricePerDay" name="pricePerDay" placeholder="price Per Day"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        <input type="text" required id="pricePerDay" name="pricePerDay" placeholder="Price Per Day"
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="licensePlate">
                             License plate</label> */}
-                        <input type="text" required id="licensePlate" name="licensePlate" placeholder="License plate"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        <input type="text" required id="licensePlate" name="licensePlate" placeholder="License Plate"
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="address">
                             Address</label> */}
                         <input type="text" required id="address" name="address" placeholder="Address"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="district">
                             District</label> */}
                         <input type="text" required id="district" name="district" placeholder="District"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="province">
                             Province</label> */}
                         <input type="text" required id="province" name="province" placeholder="Province"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="postalCode">
                             Postal code</label> */}
-                        <input type="text" required id="postalCode" name="postalCode" placeholder="Postal code"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        <input type="text" required id="postalCode" name="postalCode" placeholder="Postal Code"
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="googleMapsURL">
                             Google Maps URL</label> */}
                         <input type="text" required id="googleMapsURL" name="googleMapsURL" placeholder="Google Maps URL"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="imageURL">
                             Image URL</label> */}
                         <input type="text" required id="imageURL" name="imageURL" placeholder="Image URL"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
                     <div className="flex items-center w-full my-2">
                         {/* <label className="w-auto block text-white pr-4" htmlFor="tel">
                             Tel</label> */}
-                        <input type="text" required id="tel" name="tel" placeholder="tel"
-                        className="bg-white border-2 border-gray-200 rounded w-full p-2
+                        <input type="text" required id="tel" name="tel" placeholder="Tel"
+                        className="bg-rose-200 border-2 border-gray-200 rounded w-full p-2
                         text-white focus:outline-none focus:border-blue-400"/>
                     </div>
 
-                    <button type="submit" className="bg-white text-red-400 w-full hover:bg-red-500 hover:text-white p-2 rounded">Add New Car</button>
+                    <button type="submit" className="bg-red-500 shadow-md text-white w-full hover:bg-rose-400 hover:text-rose-600 p-2 rounded mt-5">Add New Car</button>
                 </form>
+                </div>
+                </div>
                 : null
             }
 
